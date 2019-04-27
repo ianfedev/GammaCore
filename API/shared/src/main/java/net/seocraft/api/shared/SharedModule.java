edc40088ab754ca37892a899b23e496e7380546f
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-04-27 13:50:40 -0500

    bind the messager and add a requireBinding statement
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/shared/src/main/java/net/seocraft/api/shared/SharedModule.java
+++ API/shared/src/main/java/net/seocraft/api/shared/SharedModule.java
@@ -6,6 +6,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+import net.seocraft.api.shared.redis.Messager;
+import net.seocraft.api.shared.redis.RedisMessager;
+import redis.clients.jedis.JedisPool;
 
 import java.util.concurrent.Executors;
 
@@ -20,5 +23,8 @@ public class SharedModule extends AbstractModule {
                     .setPrettyPrinting()
                     .create();
         }).in(Scopes.SINGLETON);
+
+        requireBinding(JedisPool.class);
+        bind(Messager.class).to(RedisMessager.class).in(Scopes.SINGLETON);
     }
 }
