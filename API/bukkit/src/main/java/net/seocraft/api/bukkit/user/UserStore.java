8b79fe6d72b31eb5b2c722b24f041e8c770daf1f
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-05-05 20:45:43 -0500

    Deprecate some methods, and create a lot more
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/bukkit/src/main/java/net/seocraft/api/bukkit/user/UserStore.java
+++ API/bukkit/src/main/java/net/seocraft/api/bukkit/user/UserStore.java
@@ -2,11 +2,9 @@ package net.seocraft.api.bukkit.user;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import net.seocraft.api.bukkit.BukkitAPI;
 import net.seocraft.api.bukkit.server.ServerTokenQuery;
 import net.seocraft.api.shared.http.exceptions.BadRequest;
 import net.seocraft.api.shared.http.exceptions.InternalServerError;
@@ -17,22 +15,45 @@ import net.seocraft.api.shared.redis.RedisClient;
 import net.seocraft.api.shared.serialization.models.UserDeserializer;
 import net.seocraft.api.shared.user.UserDataRequest;
 
-import java.util.concurrent.Executors;
+import java.util.UUID;
 
 @Singleton
 public class UserStore {
 
-    @Inject private UserDataRequest request;
-    @Inject private ListeningExecutorService executorService;
-    @Inject private UserDeserializer userDeserializer;
-    @Inject private ServerTokenQuery tokenHandler;
-    @Inject private Gson gson;
-    @Inject private RedisClient client;
+    @Inject
+    private UserDataRequest request;
+    @Inject
+    private ListeningExecutorService executorService;
+    @Inject
+    private UserDeserializer userDeserializer;
+    @Inject
+    private ServerTokenQuery tokenHandler;
+    @Inject
+    private Gson gson;
+    @Inject
+    private RedisClient client;
 
+    /**
+     * @deprecated Use storeUser(User) as replacement of this
+     */
+    @Deprecated
+    public void storeUser(String name, User model) {
+        storeUser(name, gson.toJson(model));
+    }
+
+    /**
+     * @deprecated Use storeUser(User) as replacement of this
+     */
+    @Deprecated
     public void storeUser(String name, String model) {
         name = name.toLowerCase();
         this.client.setString(name, model);
         this.client.setExpiration(name, 120);
+
+        User user = gson.fromJson(model, User.class);
+        String id = user.id();
+
+        storeUser(id, user);
     }
 
     public ListenableFuture<User> getUserObject(String name) {
@@ -41,7 +62,7 @@ public class UserStore {
 
     public User getUserObjectSync(String name) {
         name = name.toLowerCase();
-        if (this.client.existsKey(name)) {
+        if (this.client.existsKey("user.name:" + name)) {
             return this.gson.fromJson(this.client.getString(name), User.class);
         } else {
             return getUser(name);
@@ -53,11 +74,49 @@ public class UserStore {
             User deserializeUser = this.userDeserializer.deserializeModel(
                     this.request.executeRequest(name, this.tokenHandler.getToken())
             );
-            storeUser(name, this.gson.toJson(deserializeUser, User.class));
+
+            storeUser(deserializeUser);
             return deserializeUser;
         } catch (Unauthorized | InternalServerError | NotFound | BadRequest error) {
             return null;
         }
     }
 
+
+    public void storeUser(User model) {
+        this.client.setString("user:" + model.id(), gson.toJson(model));
+        this.client.setExpiration(model.id(), 120);
+
+        this.client.setString("user.name:" + model.getUsername(), gson.toJson(model));
+        this.client.setExpiration(model.id(), 120);
+    }
+
+    public ListenableFuture<User> getUserObject(UUID id) {
+        return this.executorService.submit(() -> getUserObjectSync(id));
+    }
+
+    public User getUserObjectSync(UUID id) {
+        String idString = id.toString();
+
+        if (this.client.existsKey("user:" + idString)) {
+            return this.gson.fromJson(this.client.getString("user:" + idString), User.class);
+        } else {
+            return getUser(id);
+        }
+    }
+
+    private User getUser(UUID id) {
+        String idString = id.toString();
+
+        try {
+            User deserializeUser = this.userDeserializer.deserializeModel(
+                    this.request.executeRequest(idString, this.tokenHandler.getToken())
+            );
+
+            storeUser(deserializeUser);
+            return deserializeUser;
+        } catch (Unauthorized | InternalServerError | NotFound | BadRequest error) {
+            return null;
+        }
+    }
 }
