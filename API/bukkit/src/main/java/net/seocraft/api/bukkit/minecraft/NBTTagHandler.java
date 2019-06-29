9c9aca4ecfdab80ae0b9c73f0e7dd7a6a9fcad7d
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-29 13:26:00 -0500

    Simplify the getTag method and add @NotNull annotations
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/bukkit/src/main/java/net/seocraft/api/bukkit/minecraft/NBTTagHandler.java
+++ API/bukkit/src/main/java/net/seocraft/api/bukkit/minecraft/NBTTagHandler.java
@@ -3,35 +3,37 @@ package net.seocraft.api.bukkit.minecraft;
 import net.minecraft.server.v1_8_R3.ItemStack;
 import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class NBTTagHandler {
 
-    private static NBTTagCompound getTag(org.bukkit.inventory.ItemStack item) {
+    @NotNull
+    private static NBTTagCompound getTag(@NotNull org.bukkit.inventory.ItemStack item) {
         ItemStack itemNms = CraftItemStack.asNMSCopy(item);
-        NBTTagCompound tag;
-        if (itemNms.hasTag()) tag = itemNms.getTag();
-        else tag = new NBTTagCompound();
-        return tag;
+
+        return itemNms.hasTag() ? itemNms.getTag() : new NBTTagCompound();
     }
 
-    private static org.bukkit.inventory.ItemStack setTag(org.bukkit.inventory.ItemStack item, NBTTagCompound tag) {
+    @NotNull
+    private static org.bukkit.inventory.ItemStack setTag(@NotNull org.bukkit.inventory.ItemStack item, NBTTagCompound tag) {
         ItemStack itemNms = CraftItemStack.asNMSCopy(item);
         itemNms.setTag(tag);
         return CraftItemStack.asBukkitCopy(itemNms);
     }
 
-    public static org.bukkit.inventory.ItemStack addString(org.bukkit.inventory.ItemStack item, String name, String value) {
+    @NotNull
+    public static org.bukkit.inventory.ItemStack addString(@NotNull org.bukkit.inventory.ItemStack item, String name, String value) {
         NBTTagCompound tag = getTag(item);
         tag.setString(name, value);
         return setTag(item, tag);
     }
 
-    public static boolean hasString(org.bukkit.inventory.ItemStack item, String name) {
+    public static boolean hasString(@NotNull org.bukkit.inventory.ItemStack item, String name) {
         NBTTagCompound tag = getTag(item);
         return tag.hasKey(name);
     }
 
-    public static String getString(org.bukkit.inventory.ItemStack item, String name) {
+    public static String getString(@NotNull org.bukkit.inventory.ItemStack item, String name) {
         NBTTagCompound tag = getTag(item);
         return tag.getString(name);
     }
