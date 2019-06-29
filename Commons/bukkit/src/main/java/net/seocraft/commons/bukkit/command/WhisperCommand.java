507c6b601854091022a0844d19740ff5fb3e3446
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-29 17:27:38 -0500

    Fixed NullPointerException when an user doesn't exist
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/command/WhisperCommand.java
+++ Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/command/WhisperCommand.java
@@ -6,6 +6,7 @@ import me.ggamer55.bcm.parametric.annotation.Command;
 import me.ggamer55.bcm.parametric.annotation.JoinedString;
 import net.seocraft.api.bukkit.user.UserStoreHandler;
 import net.seocraft.api.shared.http.AsyncResponse;
+import net.seocraft.api.shared.session.GameSession;
 import net.seocraft.api.shared.session.SessionHandler;
 import net.seocraft.api.shared.user.model.User;
 import net.seocraft.commons.bukkit.whisper.WhisperManager;
@@ -22,10 +23,14 @@ import java.util.logging.Level;
 
 public class WhisperCommand implements CommandClass {
 
-    @Inject private WhisperManager whisperManager;
-    @Inject private UserStoreHandler userStoreHandler;
-    @Inject private SessionHandler sessionHandler;
-    @Inject private TranslatableField translator;
+    @Inject
+    private WhisperManager whisperManager;
+    @Inject
+    private UserStoreHandler userStoreHandler;
+    @Inject
+    private SessionHandler sessionHandler;
+    @Inject
+    private TranslatableField translator;
 
     @Command(names = {"msg", "whisper", "tell", "w", "m", "t"}, min = 2, usage = "/<command> <target> <message...>")
     public boolean whisperCommand(CommandSender commandSender, OfflinePlayer target, @JoinedString String message) {
@@ -35,54 +40,61 @@ public class WhisperCommand implements CommandClass {
 
         Player sender = (Player) commandSender;
 
-
         CallbackWrapper.addCallback(this.userStoreHandler.getCachedUser(this.sessionHandler.getCachedSession(sender.getName()).getPlayerId()), asyncUserSender -> {
-            if (asyncUserSender.getStatus() == AsyncResponse.Status.SUCCESS) {
-                User userSender = asyncUserSender.getResponse();
-                CallbackWrapper.addCallback(this.userStoreHandler.getCachedUser(this.sessionHandler.getCachedSession(target.getName()).getPlayerId()), asyncTargetUser -> {
-                    if (asyncTargetUser.getStatus() == AsyncResponse.Status.SUCCESS) {
-                        User targetUser = asyncTargetUser.getResponse();
-                        CallbackWrapper.addCallback(whisperManager.sendMessage(userSender, targetUser, message), response -> {
-                            if (response.getResponse() == null) {
-                                ChatAlertLibrary.infoAlert(sender,
-                                        this.translator.getUnspacedField(
-                                                userSender.getLanguage(),
-                                                "commons_message_nulled"
-                                        ));
-                                return;
-                            }
+            if (asyncUserSender.getStatus() != AsyncResponse.Status.SUCCESS) {
+                ChatAlertLibrary.errorChatAlert(sender, null);
 
-                            if (response.getResponse() == WhisperResponse.Response.PLAYER_OFFLINE) {
-                                ChatAlertLibrary.infoAlert(sender,
-                                        this.translator.getUnspacedField(
-                                                userSender.getLanguage(),
-                                                "commons_player_offline"
-                                        ));
-                                return;
-                            }
+                return;
+            }
 
-                            if (response.getResponse() == WhisperResponse.Response.ERROR) {
-                                ChatAlertLibrary.errorChatAlert(sender,
-                                        this.translator.getUnspacedField(
-                                                userSender.getLanguage(),
-                                                "commons_system_error"
-                                        ));
-                                Bukkit.getLogger().log(Level.SEVERE, "An error ocurred while executing the whisper command", response.getThrowedException());
-                            }
-                        });
-                    } else {
-                        ChatAlertLibrary.errorChatAlert(
-                                sender,
-                                this.translator.getUnspacedField(userSender.getLanguage(), "commons_message_error")
-                        );
+            User userSender = asyncUserSender.getResponse();
+
+            GameSession targetSession = this.sessionHandler.getCachedSession(target.getName());
+
+            if (targetSession == null) {
+                ChatAlertLibrary.errorChatAlert(sender, this.translator.getUnspacedField(
+                        userSender.getLanguage(),
+                        "commons_not_found") + ".");
+                return;
+            }
+
+            CallbackWrapper.addCallback(this.userStoreHandler.getCachedUser(targetSession.getPlayerId()), asyncTargetUser -> {
+                if (asyncTargetUser.getStatus() != AsyncResponse.Status.SUCCESS) {
+                    ChatAlertLibrary.errorChatAlert(sender, null);
+
+                    return;
+                }
+
+                User targetUser = asyncTargetUser.getResponse();
+                CallbackWrapper.addCallback(whisperManager.sendMessage(userSender, targetUser, message), response -> {
+                    if (response.getResponse() == null) {
+                        ChatAlertLibrary.infoAlert(sender,
+                                this.translator.getUnspacedField(
+                                        userSender.getLanguage(),
+                                        "commons_message_nulled"
+                                ));
+                        return;
+                    }
+
+                    if (response.getResponse() == WhisperResponse.Response.PLAYER_OFFLINE) {
+                        ChatAlertLibrary.infoAlert(sender,
+                                this.translator.getUnspacedField(
+                                        userSender.getLanguage(),
+                                        "commons_player_offline"
+                                ));
+                        return;
+                    }
+
+                    if (response.getResponse() == WhisperResponse.Response.ERROR) {
+                        ChatAlertLibrary.errorChatAlert(sender,
+                                this.translator.getUnspacedField(
+                                        userSender.getLanguage(),
+                                        "commons_system_error"
+                                ));
+                        Bukkit.getLogger().log(Level.SEVERE, "An error ocurred while executing the whisper command", response.getThrowedException());
                     }
                 });
-            } else {
-                ChatAlertLibrary.errorChatAlert(
-                        sender,
-                        null
-                );
-            }
+            });
         });
         return true;
     }
