90361509c24f16ac50c1bb2a1d025c28c7e6516b
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-29 17:20:50 -0500

    The method getCachedSession was annotated as NotNull, but it's nullable
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/shared/src/main/java/net/seocraft/api/shared/session/SessionHandler.java
+++ API/shared/src/main/java/net/seocraft/api/shared/session/SessionHandler.java
@@ -2,12 +2,13 @@ package net.seocraft.api.shared.session;
 
 import net.seocraft.api.shared.user.model.User;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface SessionHandler {
 
     void createGameSession(@NotNull User user, String address, String version);
 
-    @NotNull GameSession getCachedSession(@NotNull String username);
+    @Nullable GameSession getCachedSession(@NotNull String username);
 
     void removeGameSession(@NotNull String username);
 }
