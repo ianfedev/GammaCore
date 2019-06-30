d83d4f8fdff028076c27a144784ae66662cb6d2f
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-30 18:15:29 -0500

    Fuck, i missed that
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/shared/src/main/java/net/seocraft/api/shared/serialization/model/ImplementedBy.java
+++ API/shared/src/main/java/net/seocraft/api/shared/serialization/model/ImplementedBy.java
@@ -1,5 +1,9 @@
 package net.seocraft.api.shared.serialization.model;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ImplementedBy {
     Class<?> value();
 }
