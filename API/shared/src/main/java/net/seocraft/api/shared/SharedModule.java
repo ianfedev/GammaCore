4b771e2b17a7348b9df665f883113c2d8cfc8638
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-07-19 17:20:20 -0500

    Create a binding for IRedisClient
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/shared/src/main/java/net/seocraft/api/shared/SharedModule.java
+++ API/shared/src/main/java/net/seocraft/api/shared/SharedModule.java
@@ -8,7 +8,9 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 import net.seocraft.api.shared.online.OnlinePlayersApi;
 import net.seocraft.api.shared.online.OnlinePlayersImpl;
+import net.seocraft.api.shared.redis.IRedisClient;
 import net.seocraft.api.shared.redis.Messager;
+import net.seocraft.api.shared.redis.RedisClientImpl;
 import net.seocraft.api.shared.redis.RedisMessager;
 import net.seocraft.api.shared.serialization.model.ModelSerializationHandler;
 import net.seocraft.api.shared.serialization.model.ModelSerializationHandlerImp;
@@ -22,8 +24,9 @@ public class SharedModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ExecutorService.class).to(ListeningExecutorService.class);
-        bind(OnlinePlayersApi.class).to(OnlinePlayersImpl.class);
         bind(ListeningExecutorService.class).toInstance(MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(8)));
+        bind(OnlinePlayersApi.class).to(OnlinePlayersImpl.class);
+        bind(IRedisClient.class).to(RedisClientImpl.class);
         bind(ModelSerializationHandler.class).to(ModelSerializationHandlerImp.class);
         bind(SessionHandler.class).to(SessionHandlerImp.class);
         bind(Messager.class).to(RedisMessager.class).in(Scopes.SINGLETON);
