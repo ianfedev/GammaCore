d50e7d47e7fe7e0ba71cf013f03ae35087524e28
Ian Castaño <ianfe@martina.com.co>
2019-07-27 22:47:33 -0500

    Updated map folder loader

---
+++ API/bukkit/src/main/java/net/seocraft/api/bukkit/map/MapFileManager.java
@@ -0,0 +1,9 @@
+package net.seocraft.api.bukkit.map;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface MapFileManager {
+
+    void configureMapFolder();
+
+}
---
+++ Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/map/CraftMapFileManager.java
@@ -0,0 +1,14 @@
+package net.seocraft.commons.bukkit.map;
+
+import net.seocraft.api.bukkit.map.MapFileManager;
+
+import java.io.File;
+
+public class CraftMapFileManager implements MapFileManager {
+
+    @Override
+    public void configureMapFolder() {
+        File mapFolder = new File();
+    }
+
+}
