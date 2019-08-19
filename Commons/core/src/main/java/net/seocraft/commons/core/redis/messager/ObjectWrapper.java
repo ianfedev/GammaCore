bd5ed11b00947fc125a7f6bbe27a55a7d9c52b64
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-08-19 00:11:52 -0500

    Make the ObjectWrapper class serializable by jackson
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- Commons/core/src/main/java/net/seocraft/commons/core/redis/messager/ObjectWrapper.java
+++ Commons/core/src/main/java/net/seocraft/commons/core/redis/messager/ObjectWrapper.java
@@ -1,12 +1,15 @@
 package net.seocraft.commons.core.redis.messager;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ObjectWrapper<O> {
 
     private final O object;
-
     private final String serverSenderId;
 
-    public ObjectWrapper(O object, String serverSenderId) {
+    @JsonCreator
+    public ObjectWrapper(@JsonProperty("object") O object, @JsonProperty("serverSenderId") String serverSenderId) {
         this.object = object;
         this.serverSenderId = serverSenderId;
     }
