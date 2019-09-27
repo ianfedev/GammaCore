be29b3d82d75a38405875c2aad94217a01cf6bfc
TomateDeveloper <ianfe@martina.com.co>
2019-09-26 23:01:37 -0500

    updated session manager

--- Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/game/management/CraftGameSessionManager.java
+++ Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/game/management/CraftGameSessionManager.java
@@ -135,7 +135,7 @@ public class CraftGameSessionManager implements GameLoginManager {
                                         )
                                         .replace(
                                                 "%%actual%%",
-                                                ChatColor.AQUA + "" + (actualPlayers - 1) + ChatColor.YELLOW
+                                                ChatColor.AQUA + "" + actualPlayers + ChatColor.YELLOW
                                         )
                                         .replace(
                                                 "%%max%%",
