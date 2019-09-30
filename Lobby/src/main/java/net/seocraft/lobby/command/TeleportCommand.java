ec46860f2654b38ba5e26a66f00fd21f02a4e4d8
TomateDeveloper <ianfe@martina.com.co>
2019-09-29 19:13:00 -0500

    IntelliJ code Cleanup
    Signed-off-by: TomateDeveloper <ianfe@martina.com.co>

--- Lobby/src/main/java/net/seocraft/lobby/command/TeleportCommand.java
+++ Lobby/src/main/java/net/seocraft/lobby/command/TeleportCommand.java
@@ -1,38 +1,19 @@
 package net.seocraft.lobby.command;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import me.fixeddev.bcm.CommandContext;
 import me.fixeddev.bcm.parametric.CommandClass;
 import me.fixeddev.bcm.parametric.annotation.Command;
 import me.fixeddev.bcm.parametric.annotation.Parameter;
-import net.seocraft.api.bukkit.cloud.CloudManager;
-import net.seocraft.api.bukkit.game.management.CoreGameManagement;
-import net.seocraft.api.bukkit.game.management.FinderResult;
-import net.seocraft.api.bukkit.game.management.MatchFinder;
-import net.seocraft.api.bukkit.stats.Stats;
-import net.seocraft.api.bukkit.stats.StatsProvider;
-import net.seocraft.api.core.http.exceptions.BadRequest;
-import net.seocraft.api.core.http.exceptions.InternalServerError;
-import net.seocraft.api.core.http.exceptions.NotFound;
-import net.seocraft.api.core.http.exceptions.Unauthorized;
-import net.seocraft.api.core.redis.RedisClient;
-import net.seocraft.api.core.session.GameSessionManager;
 import net.seocraft.api.bukkit.lobby.TeleportManager;
-import net.seocraft.api.core.user.UserStorageProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.io.IOException;
-import java.util.UUID;
-
 public class TeleportCommand implements CommandClass {
 
     @Inject private TeleportManager teleportManager;
-    @Inject private StatsProvider statsProvider;
-    @Inject private UserStorageProvider userStorageProvider;
 
     @Command(names = {"tp", "teleport", "tele"}, usage = "/<command> <target> [-s]", permission = "commons.staff.lobby.tp")
     public boolean teleportCommand(CommandSender commandSender, OfflinePlayer target, @Parameter(value = "s", isFlag =  true) boolean silent) {
@@ -75,18 +56,5 @@ public class TeleportCommand implements CommandClass {
         }
         return true;
     }
-
-    @Command(names = {"test"})
-    public boolean testCommand(CommandSender sender) {
-        try {
-            Stats playerStats = this.statsProvider.getPlayerStats(this.userStorageProvider.findUserByIdSync("5d8e8bf11af627456703dd93"));
-            playerStats.getSkyWarsStats().addCoins(15);
-            this.statsProvider.updatePlayerStats(playerStats);
-        } catch (Unauthorized | InternalServerError | BadRequest | NotFound | IOException unauthorized) {
-            unauthorized.printStackTrace();
-        }
-        return true;
-    }
-
 
 }
