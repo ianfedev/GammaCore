5db2a5b66464386e3bfffccfd207fe516a134837
TomateDeveloper <ianfe@martina.com.co>
2019-11-26 12:44:09 -0500

    Updated Game Finished event

--- API/bukkit/src/main/java/net/seocraft/api/bukkit/event/GameFinishedEvent.java
+++ API/bukkit/src/main/java/net/seocraft/api/bukkit/event/GameFinishedEvent.java
@@ -2,8 +2,6 @@ package net.seocraft.api.bukkit.event;
 
 import net.seocraft.api.bukkit.game.match.Match;
 import net.seocraft.api.bukkit.game.match.partial.Team;
-import net.seocraft.api.core.user.User;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -13,14 +11,10 @@ public class GameFinishedEvent extends Event {
 
     private static HandlerList handlerList = new HandlerList();
     @NotNull private Match match;
-    @NotNull private Player player;
-    @NotNull private User user;
     @Nullable private Team winnerTeam;
 
-    public GameFinishedEvent(@NotNull Match match, @NotNull Player player, @NotNull User user, @Nullable Team winnerTeam) {
+    public GameFinishedEvent(@NotNull Match match, @Nullable Team winnerTeam) {
         this.match = match;
-        this.player = player;
-        this.user = user;
         this.winnerTeam = winnerTeam;
     }
 
@@ -35,14 +29,6 @@ public class GameFinishedEvent extends Event {
 
     public @NotNull Match getMatch() {
         return this.match;
-    }
-
-    public @NotNull Player getPlayer() {
-        return this.player;
-    }
-
-    public @NotNull User getUser() {
-        return this.user;
     }
 
     public @Nullable Team getWinnerTeam() {
