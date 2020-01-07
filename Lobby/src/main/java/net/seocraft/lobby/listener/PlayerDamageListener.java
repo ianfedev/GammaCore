026310008b1e73d7c228a646e4660192c16476be
TomateDeveloper <ianfe@martina.com.co>
2020-01-07 16:23:41 -0500

    Updated gamemode translations

--- Lobby/src/main/java/net/seocraft/lobby/listener/PlayerDamageListener.java
+++ Lobby/src/main/java/net/seocraft/lobby/listener/PlayerDamageListener.java
@@ -3,13 +3,16 @@ package net.seocraft.lobby.listener;
 import com.google.inject.Inject;
 import net.seocraft.api.bukkit.lobby.TeleportManager;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class PlayerDamageListener implements Listener {
 
     @Inject private TeleportManager teleportManager;
 
+    @EventHandler
     public void playerDeathEvent(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
@@ -20,6 +23,11 @@ public class PlayerDamageListener implements Listener {
 
             event.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    public void entityDamageEvent(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player) event.setCancelled(true);
     }
 
 }
