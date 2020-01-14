ac4f4cafa3e2e70bd195d187552acfc25c680396
TomateDeveloper <ianfe@martina.com.co>
2020-01-14 14:13:46 -0500

    Add for serializer

--- Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/serializer/minecraft/EnchantmentDeserializer.java
+++ Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/serializer/minecraft/EnchantmentDeserializer.java
@@ -22,60 +22,11 @@ public class EnchantmentDeserializer extends StdDeserializer<Enchantment> {
     public Enchantment deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
         String finalCodec = node.toString().toUpperCase();
-        System.out.println(finalCodec);
-        switch (finalCodec) {
-            case "PROTECTION_ENVIRONMENTAL":
-                return Enchantment.PROTECTION_ENVIRONMENTAL;
-            case "PROTECTION_FIRE":
-                return Enchantment.PROTECTION_FIRE;
-            case "PROTECTION_FALL":
-                return Enchantment.PROTECTION_FALL;
-            case "PROTECTION_EXPLOSIONS":
-                return Enchantment.PROTECTION_EXPLOSIONS;
-            case "PROTECTION_PROJECTILE":
-                return Enchantment.PROTECTION_PROJECTILE;
-            case "OXYGEN":
-                return Enchantment.OXYGEN;
-            case "WATER_WORKER":
-                return Enchantment.WATER_WORKER;
-            case "THORNS" :
-                return Enchantment.THORNS;
-            case "DEPTH_STRIDER":
-                return Enchantment.DEPTH_STRIDER;
-            case "DAMAGE_ALL":
-                return Enchantment.DAMAGE_ALL;
-            case "DAMAGE_UNDEAD":
-                return Enchantment.DAMAGE_UNDEAD;
-            case "DAMAGE_ARTHROPODS":
-                return Enchantment.DAMAGE_ARTHROPODS;
-            case "KNOCKBACK":
-                return Enchantment.KNOCKBACK;
-            case "FIRE_ASPECT":
-                return Enchantment.FIRE_ASPECT;
-            case "LOOT_BONUS_MOBS":
-                return Enchantment.LOOT_BONUS_MOBS;
-            case "DIG_SPEED":
-                return Enchantment.DIG_SPEED;
-            case "SILK_TOUCH":
-                return Enchantment.SILK_TOUCH;
-            case "DURABILITY":
-                return Enchantment.DURABILITY;
-            case "LOOT_BONUS_BLOCKS":
-                return Enchantment.LOOT_BONUS_BLOCKS;
-            case "ARROW_DAMAGE":
-                return Enchantment.ARROW_DAMAGE;
-            case "ARROW_KNOCKBACK":
-                return Enchantment.ARROW_KNOCKBACK;
-            case "ARROW_FIRE":
-                return Enchantment.ARROW_FIRE;
-            case "ARROW_INFINITE":
-                return Enchantment.ARROW_INFINITE;
-            case "LUCK":
-                return Enchantment.LUCK;
-            case "LURE":
-                return Enchantment.LURE;
-            default:
-                return Enchantment.getByName(node.toString().toUpperCase());
+        for (Enchantment enchant : Enchantment.values()) {
+            System.out.println(enchant.getName());
+            System.out.println(finalCodec.toUpperCase());
+            if (finalCodec.equalsIgnoreCase(enchant.getName())) return enchant;
         }
+        return null;
     }
 }
