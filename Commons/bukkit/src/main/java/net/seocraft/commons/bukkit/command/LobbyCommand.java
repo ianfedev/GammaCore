e2017de7eccfe83b105856142b93b4c314d51cda
TomateDeveloper <ianfe@martina.com.co>
2020-04-04 20:43:18 -0500

    Added lobby command

--- Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/command/LobbyCommand.java
+++ Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/command/LobbyCommand.java
@@ -1,17 +1,27 @@
 package net.seocraft.commons.bukkit.command;
 
+import com.google.inject.Inject;
 import me.fixeddev.bcm.parametric.CommandClass;
 import me.fixeddev.bcm.parametric.annotation.Command;
-import org.bukkit.Bukkit;
+import net.seocraft.api.bukkit.cloud.CloudManager;
+import net.seocraft.api.bukkit.game.management.CoreGameManagement;
+import net.seocraft.api.core.server.ServerType;
+import net.seocraft.commons.bukkit.CommonsBukkit;
 import org.bukkit.command.CommandSender;
-import java.util.logging.Level;
+import org.bukkit.entity.Player;
 
 public class LobbyCommand implements CommandClass {
 
+    @Inject private CommonsBukkit instance;
+    @Inject private CloudManager cloudManager;
+    @Inject private CoreGameManagement coreGameManagement;
 
     @Command(names = {"lobby", "l", "hub"})
     public boolean mainCommand(CommandSender commandSender) {
-        Bukkit.getLogger().log(Level.INFO, "Successfuckly");
+        String game = "main_lobby";
+        if (this.instance.getServerRecord().getServerType() == ServerType.GAME)
+            game = this.coreGameManagement.getGamemode().getLobbyGroup();
+        this.cloudManager.sendPlayerToGroup((Player) commandSender, game);
         return true;
     }
 }
