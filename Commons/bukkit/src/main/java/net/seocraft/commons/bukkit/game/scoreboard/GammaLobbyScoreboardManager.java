58fdae10c62cb4b46ed447dfbacf5d26f444af86
TomateDeveloper <ianfe@martina.com.co>
2020-04-09 23:45:47 -0500

    Updated

--- Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/game/scoreboard/GammaLobbyScoreboardManager.java
+++ Commons/bukkit/src/main/java/net/seocraft/commons/bukkit/game/scoreboard/GammaLobbyScoreboardManager.java
@@ -38,75 +38,74 @@ public class GammaLobbyScoreboardManager implements LobbyScoreboardManager {
             if (player != null) {
 
                 Scoreboard scoreboard = scoreboardMap.get(user.getId());
-                if (!scoreboardMap.containsKey(user.getId())) {
-                    scoreboard = this.scoreboardManager.createScoreboard(
-                            ChatColor.GOLD + "" + ChatColor.BOLD + this.translatableField.getUnspacedField(
-                                    user.getLanguage(),
-                                    this.coreGameManagement.getGamemode().getName().toUpperCase()
-                            )
-                    );
-                    scoreboard.apply(player);
-                    scoreboardMap.put(user.getId(), scoreboard);
+                if (scoreboardMap.containsKey(user.getId())) {
+                    scoreboard.getRemover().remove(scoreboard, player);
                 }
+                scoreboard = this.scoreboardManager.createScoreboard(
+                        ChatColor.GOLD + "" + ChatColor.BOLD + this.translatableField.getUnspacedField(
+                                user.getLanguage(),
+                                this.coreGameManagement.getGamemode().getName().toUpperCase()
+                        )
+                );
+                scoreboard.apply(player);
+                scoreboardMap.put(user.getId(), scoreboard);
 
-                if (scoreboard != null) {
-                    scoreboard.setLine(10,ChatColor.RED + " ");
+                scoreboard.setLine(10,ChatColor.RED + " ");
+                scoreboard.setLine(
+                        9,
+                        ChatColor.YELLOW + this.translatableField.getUnspacedField(
+                                user.getLanguage(),
+                                "commons_scoreboard_players"
+                        )
+                );
+                scoreboard.setLine(
+                        8,
+                        ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE +
+                                this.coreGameManagement.getMatchPlayers(match.getId()).size() + "/" + this.coreGameManagement.getSubGamemode().getMaxPlayers()
+                );
+                scoreboard.setLine(
+                        7,
+                        ChatColor.YELLOW + this.translatableField.getUnspacedField(
+                                user.getLanguage(),
+                                "commons_scoreboard_starting"
+                        )
+                );
+                if (this.coreGameManagement.hasRemainingTime(match.getId()) && this.coreGameManagement.getRemainingTime(match.getId()) != -1) {
                     scoreboard.setLine(
-                            9,
-                            ChatColor.YELLOW + this.translatableField.getUnspacedField(
-                                    user.getLanguage(),
-                                    "commons_scoreboard_players"
-                            )
-                    );
-                    scoreboard.setLine(
-                            8,
+                            6,
                             ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE +
-                                   this.coreGameManagement.getMatchPlayers(match.getId()).size() + "/" + this.coreGameManagement.getSubGamemode().getMaxPlayers()
+                                    this.coreGameManagement.getRemainingTime(match.getId()) + "s"
                     );
+                } else {
                     scoreboard.setLine(
-                            7,
-                            ChatColor.YELLOW + this.translatableField.getUnspacedField(
-                                    user.getLanguage(),
-                                    "commons_scoreboard_starting"
-                            )
+                            6,
+                            ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE +
+                                    this.translatableField.getUnspacedField(user.getLanguage(), "commons_scoreboard_insufficent")
                     );
-                    if (this.coreGameManagement.hasRemainingTime(match.getId()) && this.coreGameManagement.getRemainingTime(match.getId()) != -1) {
-                        scoreboard.setLine(
-                                6,
-                                ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE +
-                                        this.coreGameManagement.getRemainingTime(match.getId()) + "s"
-                        );
-                    } else {
-                        scoreboard.setLine(
-                                6,
-                                ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE +
-                                        this.translatableField.getUnspacedField(user.getLanguage(), "commons_scoreboard_insufficent")
-                        );
-                    }
-                    scoreboard.setLine(
-                            5,
-                            ChatColor.YELLOW + this.translatableField.getUnspacedField(
-                                    user.getLanguage(),
-                                    "commons_scoreboard_map"
-                            )
-                    );
-                    scoreboard.setLine(
-                            4,
-                            ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE + this.coreGameManagement.getMatchMap(match).getName()
-                    );
-                    scoreboard.setLine(
-                            3,
-                            ChatColor.YELLOW + this.translatableField.getUnspacedField(
-                                    user.getLanguage(),
-                                    "commons_scoreboard_mode"
-                            )
-                    );
-                    scoreboard.setLine(2, ChatColor.YELLOW + "\u00BB " + ChatColor.ITALIC + ChatColor.WHITE +
-                            StringUtils.capitalizeString(this.coreGameManagement.getSubGamemode().getName().toLowerCase().replace("_", " "))
-                    );
-                    scoreboard.setLine(1, ChatColor.AQUA + " ");
-                    scoreboard.setLine(0, ChatColor.YELLOW + "www.seocraft.net");
                 }
+                scoreboard.setLine(
+                        5,
+                        ChatColor.YELLOW + this.translatableField.getUnspacedField(
+                                user.getLanguage(),
+                                "commons_scoreboard_map"
+                        )
+                );
+                scoreboard.setLine(
+                        4,
+                        ChatColor.YELLOW + "\u00BB " + ChatColor.GREEN + ChatColor.WHITE + this.coreGameManagement.getMatchMap(match).getName()
+                );
+                scoreboard.setLine(
+                        3,
+                        ChatColor.YELLOW + this.translatableField.getUnspacedField(
+                                user.getLanguage(),
+                                "commons_scoreboard_mode"
+                        )
+                );
+                scoreboard.setLine(2, ChatColor.YELLOW + "\u00BB " + ChatColor.ITALIC + ChatColor.WHITE +
+                        StringUtils.capitalizeString(this.coreGameManagement.getSubGamemode().getName().toLowerCase().replace("_", " "))
+                );
+                scoreboard.setLine(1, ChatColor.AQUA + " ");
+                scoreboard.setLine(0, ChatColor.YELLOW + "www.seocraft.net");
 
             }
         });
