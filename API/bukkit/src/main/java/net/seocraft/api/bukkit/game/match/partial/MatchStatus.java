2ecc2a1ca4f02bb6d162b9b9f6d172895bf25acb
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-04-23 20:22:51 -0500

    Added the PREPARING match state
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/bukkit/src/main/java/net/seocraft/api/bukkit/game/match/partial/MatchStatus.java
+++ API/bukkit/src/main/java/net/seocraft/api/bukkit/game/match/partial/MatchStatus.java
@@ -3,6 +3,8 @@ package net.seocraft.api.bukkit.game.match.partial;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum MatchStatus {
+    @JsonProperty("preparing")
+    PREPARING,
     @JsonProperty("waiting")
     WAITING,
     @JsonProperty("starting")
