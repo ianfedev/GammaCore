2b84c0c9d94a55af52f0c16101cdebddbf219cc4
Ian Castaño <ianfe@martina.com.co>
2020-05-13 11:14:02 -0500

    Fixed punishment type

--- API/bukkit/src/main/java/net/seocraft/api/bukkit/punishment/PunishmentType.java
+++ API/bukkit/src/main/java/net/seocraft/api/bukkit/punishment/PunishmentType.java
@@ -3,7 +3,7 @@ package net.seocraft.api.bukkit.punishment;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum PunishmentType {
-    @JsonProperty("ban") BAN,
-    @JsonProperty("kick") KICK,
-    @JsonProperty("warn") WARN
+    @JsonProperty("Ban") BAN,
+    @JsonProperty("Kick") KICK,
+    @JsonProperty("Warn") WARN
 }
